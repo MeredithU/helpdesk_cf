26c9af9eba797f8c6dc1b230c033da78683304b0
Meredith Underell <m.underell@gmail.com>
2013-08-08 12:47:20 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HelpdeskCf::Application.config.secret_token = '03d2f34564b06808e136bd39a8b16b1344d15adafdc47c7365f592c7fc5b5d4c48b556a7f29be56ec316b41190ae3d3884d1936ff1c906f66187d43f4bc6c65e'
